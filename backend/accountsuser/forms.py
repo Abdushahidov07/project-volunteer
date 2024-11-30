3c8cdccec359f00ebb40ae3584a217faa9a10996
Abdullo <147158343+Abdushahidov07@users.noreply.github.com>
2024-11-30 11:54:30 +0500

    New Models

--- backend/accountsuser/forms.py
+++ backend/accountsuser/forms.py
@@ -3,8 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from volunteer.models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    wallet_balance = forms.DecimalField(max_digits=10, decimal_places=2, initial=0.00, required=False)
-
     class Meta:
         model = CustomUser
-        fields = ['username', 'password1', 'password2']
+        fields = ['username','first_name','last_name','age','phone_number','gender','status','password1', 'password2']
